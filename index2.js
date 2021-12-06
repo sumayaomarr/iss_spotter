2fd341fe1b4804b22120994f2a4c419ed82c7bff
sumaya <sumayaomarr@gmail.com>
2021-12-06 01:43:09 -0500

    iss

---
+++ index2.js
@@ -0,0 +1,10 @@
+const { nextISSTimesForMyLocation, printPassTimes } = require('./iss_promised');
+const { fetchMyIP } = require('./iss_promised');
+
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  }).catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
